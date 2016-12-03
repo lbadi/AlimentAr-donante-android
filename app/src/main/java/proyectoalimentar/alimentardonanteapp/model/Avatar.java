a0ddfe60b6287b08b3189569c2acc15e04488f97
Leonel Badi <lbadi@itba.edu.ar>
2016-12-03 00:06:28 -0300

    Advances in profile fragment

---
+++ app/src/main/java/proyectoalimentar/alimentardonanteapp/model/Avatar.java
@@ -0,0 +1,8 @@
+package proyectoalimentar.alimentardonanteapp.model;
+
+/**
+ * Created by leonelbadi on 2/12/16.
+ */
+
+public class Avatar {
+}
---
+++ app/src/main/java/proyectoalimentar/alimentardonanteapp/ui/profile/ProfileFragment.java
@@ -0,0 +1,8 @@
+package proyectoalimentar.alimentardonanteapp.ui.profile;
+
+/**
+ * Created by leonelbadi on 2/12/16.
+ */
+
+public class ProfileFragment {
+}
